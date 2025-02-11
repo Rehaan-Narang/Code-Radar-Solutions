87668f4fedfa74a42807a419981005a87d2d4a6b
Rehaan Narang <195541500+Rehaan-Narang@users.noreply.github.com>
2025-02-11 17:05:55 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -3,6 +3,7 @@ int main(){
     int a,b;
     char c;
     scanf("%d %d %c", &a,&b , &c);
+    //method 1 = switch case
     if(c=='/' && b!=0){
         printf("%d", a/b);
     }
@@ -17,6 +18,8 @@ int main(){
         default: printf("error");
     }
     }
+
+//method 2 = if else
 
     // if(c=='+'){
     //     printf("%d", a+b);
