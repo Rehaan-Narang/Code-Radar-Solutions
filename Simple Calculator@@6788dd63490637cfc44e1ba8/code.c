51b87e04b3b4a455bc0136e8b7533aef4725bc80
Rehaan Narang <195541500+Rehaan-Narang@users.noreply.github.com>
2025-02-11 17:04:53 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -3,31 +3,34 @@ int main(){
     int a,b;
     char c;
     scanf("%d %d %c", &a,&b , &c);
-    // switch(c){
-    //     case'+': printf("%d", a+b);
-    //     break;
-    //     case '-': printf("%d", a-b);
-    //     break;
-    //     case '*': printf("%d", a*b);
-    //     break;
-    //     case '/': printf("%d", a/b);
-    //     break;
-    //     default: printf("error");
-    // }
-
-    if(c=='+'){
-        printf("%d", a+b);
-    }
-    else if(c=='-'){
-        printf("%d", a-b);
-    }
-    else if(c=='*'){
-        printf("%d", a*b);
-    }
-    else if(c=='/' && b!=0){
+    if(c=='/' && b!=0){
         printf("%d", a/b);
     }
     else{
-        printf("error");
+    switch(c){
+        case'+': printf("%d", a+b);
+        break;
+        case '-': printf("%d", a-b);
+        break;
+        case '*': printf("%d", a*b);
+        break;
+        default: printf("error");
     }
+    }
+
+    // if(c=='+'){
+    //     printf("%d", a+b);
+    // }
+    // else if(c=='-'){
+    //     printf("%d", a-b);
+    // }
+    // else if(c=='*'){
+    //     printf("%d", a*b);
+    // }
+    // else if(c=='/' && b!=0){
+    //     printf("%d", a/b);
+    // }
+    // else{
+    //     printf("error");
+    // }
 }
