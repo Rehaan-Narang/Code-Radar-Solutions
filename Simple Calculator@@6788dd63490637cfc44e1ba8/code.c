8b3a61f9ca5f44d6373bd85f2867d7f4bd0ca60f
Rehaan Narang <195541500+Rehaan-Narang@users.noreply.github.com>
2025-02-18 01:40:57 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -1,24 +1,39 @@
 #include<stdio.h>
 int main()
 {
-    int x,y;
+    int x, y;
     char z;
-    scanf("%d %d",&x,&y);
-    getchar();
-    scanf("%c",&z);
-    if(z=='+'){
-        printf("%d",x+y);
+    
+    // Read two integers
+    scanf("%d %d", &x, &y);
+    
+    // Clear the input buffer by calling getchar() to consume the newline left by previous scanf
+    getchar(); 
+    
+    // Read the operator
+    scanf("%c", &z);  
+    
+    // Perform the operation based on the operator
+    if(z == '+'){
+        printf("%d", x + y);
     }
-    else if(z=='-'){
-        printf("%d",x-y);
+    else if(z == '-'){
+        printf("%d", x - y);
     }
-    else if(z=='*'){
-        printf("%d",x*y);
+    else if(z == '*'){
+        printf("%d", x * y);
     }
-    else if(z=='/'){
-        printf("%d",x/y);
+    else if(z == '/'){
+        // To prevent division by zero error
+        if (y != 0) {
+            printf("%d", x / y);
+        } else {
+            printf("error");
+        }
     }
-    else{
+    else {
         printf("error");
     }
+
+    return 0;
 }
