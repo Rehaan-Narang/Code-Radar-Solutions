8b7f929c32e21e439589b0ee3565aee0a61fc24a
Rehaan Narang <rehaannarang1010@gmail.com>
2025-04-09 20:45:16 +0530

    Solution to Remove All Spaces from a String

---
+++ Remove All Spaces from a String@@676c039d80e1714600f6764d/code.c
@@ -0,0 +1,20 @@
+// Your code here...#include <stdio.h>
+#include <string.h>
+void removeSpaces(char *str) {
+    int i, j = 0;
+    for (i = 0; str[i] != '\0'; i++) {
+        if (str[i] != ' ') {
+            str[j++] = str[i];
+        }
+    }
+    str[j] = '\0';
+}
+int main() {
+    char str[100];
+    printf("");
+    fgets(str, sizeof(str), stdin);
+    str[strcspn(str, "\n")] = 0;
+    removeSpaces(str);
+    printf("%s\n", str);
+    return 0;
+}
